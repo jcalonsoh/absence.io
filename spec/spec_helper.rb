12434f98d3257bda3de91bae91034f4b8547dd19
jcalonsoh <j.carlos.alonso.h@outlook.cl>
2016-09-08 14:03:27 -0300

    simplecov to codecov format

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
 require 'codecov'
 require_relative '../lib/absence'
+
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
